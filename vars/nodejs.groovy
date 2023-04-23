a0af33d571af7f2d777b37aef90dcc2203fa0755
unknown <anilkumarravuri510@gmail.com>
2023-04-23 20:28:41 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -24,27 +24,27 @@ def call(COMPONENT) {
          }
 }
 
-@Library('roboshop-shared-library') _
-pipeline{
-    agent any
-     stages {
-                stage ('performing the lint checks') {
-            steps{
-                script {
-                    sample.info('sharedlibrary' , 'stage.google.com')
-                }
-                   // sh "echo installing jslint"
-                   // sh "npm install jslint"
-                   // sh "ls -ltr node-modules/jslint/bin/"
-                   // sh "/home/centos/jslint/bin/jslint.js server.js"
-                // sh "echo performing lint checks"
-                // sh "echo performing lint checks completed"
-            }
-        }
-        stage ('downloading the dependencies') {
-            steps{
-                sh "npm install"
-            }
-        }
-     }
-}
+// @Library('roboshop-shared-library') _
+// pipeline{
+//     agent any
+//      stages {
+//                 stage ('performing the lint checks') {
+//             steps{
+//                 script {
+//                     sample.info('sharedlibrary' , 'stage.google.com')
+//                 }
+//                    // sh "echo installing jslint"
+//                    // sh "npm install jslint"
+//                    // sh "ls -ltr node-modules/jslint/bin/"
+//                    // sh "/home/centos/jslint/bin/jslint.js server.js"
+//                 // sh "echo performing lint checks"
+//                 // sh "echo performing lint checks completed"
+//             }
+//         }
+//         stage ('downloading the dependencies') {
+//             steps{
+//                 sh "npm install"
+//             }
+//         }
+//      }
+// }
