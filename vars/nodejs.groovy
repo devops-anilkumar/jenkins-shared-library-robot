4e64ef1fca42d8165d34355eed88c956329b4dac
unknown <anilkumarravuri510@gmail.com>
2023-04-24 17:31:21 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,7 +14,7 @@ def sonarchecks() {
     sh '''
       sonar-scanner -Dsonar.host.url=http://172.31.0.15:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CREDENTIALS_USR} -Dsonar.password=${SONAR_CREDENTIALS_PSW}
       https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh
-      bash -x quality-gate.sh ${SONAR_CREDENTIALS_PSW} ${SONAR_CREDENTIALS_USR} ${SONAR_URL} ${COMPONENT}
+      bash -x quality-gate.sh ${SONAR_CREDENTIALS_USR} ${SONAR_CREDENTIALS_PSW} ${SONAR_URL} ${COMPONENT}
      '''
 }
 // call is the default function which will be called when you call the filename
