63ef7c3ae178b097975268d927e2040771aadaed
unknown <anilkumarravuri510@gmail.com>
2023-04-24 17:33:03 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -13,7 +13,7 @@ def lintchecks() {
 def sonarchecks() {
     sh '''
       sonar-scanner -Dsonar.host.url=http://172.31.0.15:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CREDENTIALS_USR} -Dsonar.password=${SONAR_CREDENTIALS_PSW}
-      https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh
+      curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh
       bash -x quality-gate.sh ${SONAR_CREDENTIALS_USR} ${SONAR_CREDENTIALS_PSW} ${SONAR_URL} ${COMPONENT}
      '''
 }
