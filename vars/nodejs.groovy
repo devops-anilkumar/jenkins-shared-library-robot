acd0a432be0963deaa7c06ad6f638d1b1aa24942
unknown <anilkumarravuri510@gmail.com>
2023-04-24 13:42:41 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -12,7 +12,7 @@ def lintchecks() {
 
 def sonarchecks() {
     sh '''
-      sonar-scanner -Dsonar.host.url=http://172.31.0.15:9000 -Dsonar.sources=. -Dsonar.projectKey==${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
+      sonar-scanner -Dsonar.host.url=http://172.31.0.15:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
 
      '''
 }
