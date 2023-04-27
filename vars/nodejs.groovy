c628b3e68884040de6f87f1787e617e27b77e74a
unknown <anilkumarravuri510@gmail.com>
2023-04-27 19:14:11 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -66,18 +66,18 @@ def call() {
              }
           }
        }
-        stage('checking the artifacts version') {
-       when { expression { env.TAG_NAME != null} }
-           steps {
-              script{
-                env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true')
-                print UPLOAD_STATUS
-              }
-          }
-       }
+    //     stage('checking the artifacts version') {
+    //    when { expression { env.TAG_NAME != null} }
+    //        steps {
+    //           script{
+    //             env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true')
+    //             print UPLOAD_STATUS
+    //           }
+    //       }
+    //    }
        stage('prepare the artifacts') {
        when { expression { env.TAG_NAME != null}
-              expression { env.UPLOAD_STATUS == "" }
+              //expression { env.UPLOAD_STATUS == "" }
         }
            steps {
                sh "npm install"
