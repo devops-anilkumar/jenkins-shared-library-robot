8000ccf10a892fb4fe836016d08224c3d78f700a
unknown <anilkumarravuri510@gmail.com>
2023-04-28 00:04:35 +0530

    auto push

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,7 +1,7 @@
 
 def call() {
       node {
-          git branch: 'main', url: 'https://github.com/devops-anilkumar/${COMPONENT}.git'
+          git branch: 'main', url: "https://github.com/devops-anilkumar/${COMPONENT}.git"
           env.APPTYPE ="python"
           common.lintchecks()
           env.ARGS="-Dsonar.sources=."
